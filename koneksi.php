067b583190e6c3475db244fc411ecf9cbadbdb61
bila cintaaaaa <nabilaislamicinta@gmail.com>
2026-06-18 14:28:54 +0700

    [Tahap 3] Membuat File Koneksi

---
+++ koneksi.php
@@ -0,0 +1,37 @@
+<?php
+
+class Koneksi
+{
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "db_simulasi_pbo_ti1d_nabilaislamicintaw";
+
+    protected $conn;
+
+    public function __construct()
+    {
+        $this->conn = new mysqli(
+            $this->host,
+            $this->username,
+            $this->password,
+            $this->database
+        );
+
+        if ($this->conn->connect_error) {
+            die("Koneksi gagal : " . $this->conn->connect_error);
+        }
+
+        echo "Koneksi berhasil<br>";
+    }
+
+    public function getKoneksi()
+    {
+        return $this->conn;
+    }
+}
+
+// Membuat objek koneksi
+$tesKoneksi = new Koneksi();
+
+?>
