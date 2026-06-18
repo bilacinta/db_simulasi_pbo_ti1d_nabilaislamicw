547d2fb4e703291aad5d8a83297058f2bbb06220
bila cintaaaaa <nabilaislamicinta@gmail.com>
2026-06-18 15:18:44 +0700

    [Tahap 4] Implementasi Pewarisan

---
+++ pendaftarankedinasan.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'pendaftaran.php';
+
+class PendaftaranPrestasi extends Pendaftaran
+{
+    private $jenisPrestasi;
+    private $tingkatPrestasi;
+
+    public function __construct(
+        $id,
+        $nama,
+        $asal,
+        $nilai,
+        $biaya,
+        $jenisPrestasi,
+        $tingkatPrestasi
+    ){
+        parent::__construct($id, $nama, $asal, $nilai, $biaya);
+
+        $this->jenisPrestasi = $jenisPrestasi;
+        $this->tingkatPrestasi = $tingkatPrestasi;
+    }
+
+    public function hitungTotalBiaya()
+    {
+        return $this->biayaPendaftaranDasar - 100000;
+    }
+
+    public function tampilkanInfoJalur()
+    {
+        return "Prestasi : ".$this->jenisPrestasi.
+               " | Tingkat : ".$this->tingkatPrestasi;
+    }
+
+    public function getDaftarPrestasi($db)
+    {
+        $query = "SELECT * FROM tabel_pendaftaran
+                  WHERE jalur_pendaftaran='Prestasi'";
+
+        return $db->query($query);
+    }
+}
+?>
